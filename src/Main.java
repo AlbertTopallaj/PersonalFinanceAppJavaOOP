73d51ecf1e1b8953fe34e498a7e1e2e601b146fd
Professor <superelitealfa@gmail.com>
2025-10-19 22:39:29 +0200

    init

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import services.SystemService;
+
+public class Main {
+    public static void main(String[] args) {
+
+        SystemService system = new SystemService();
+
+        system.run();
+
+    }
+}
---
+++ src/commands/Command.java
@@ -0,0 +1,17 @@
+package commands;
+
+public class Command {
+
+    String name;
+    String description;
+
+    public Command(String name, String description){
+
+        this.name = name;
+        this.description = description;
+
+    }
+
+
+
+}
