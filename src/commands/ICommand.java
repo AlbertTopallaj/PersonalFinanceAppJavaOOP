7fae80a5ddb256d94897d5a4b386d6e3de6403bd
Professor <superelitealfa@gmail.com>
2025-11-06 21:56:15 +0100

    added comments for clarity

--- src/commands/ICommand.java
+++ src/commands/ICommand.java
@@ -2,7 +2,10 @@ package commands;
 
 public interface ICommand {
 
+    // Två metoder som alla kommandon har
+    // Alla kommandon utgår ifrån detta Interface
+
     void registerCommand(Command command);
-    void executeCommand(String commandInput);
+    void executeCommand(String commandInput) throws Exception;
 
 }
