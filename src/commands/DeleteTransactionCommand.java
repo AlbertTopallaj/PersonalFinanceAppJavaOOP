21e446bae1b85ca898e6b87b721c5a4448320d5e
Professor <superelitealfa@gmail.com>
2025-11-06 22:11:13 +0100

    added comments for clarity, added exception handling

--- src/commands/DeleteTransactionCommand.java
+++ src/commands/DeleteTransactionCommand.java
@@ -1,64 +1,76 @@
 package commands;
 
 import Transaction.Transaction;
+import services.ITransactionService;
 
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.UUID;
 
-public class DeleteTransactionCommand extends Command {
+public class DeleteTransactionCommand extends Command { // Ärver från kommando
 
-    protected final ArrayList<Transaction> transactions;
-
-    public DeleteTransactionCommand(ArrayList<Transaction> transactions) {
-        super("radera", "Radera en befintlig transaktion");
-        this.transactions = transactions;
+    public DeleteTransactionCommand(ITransactionService transactionService) { // Konstruktor för kommandot
+        // Namn samt beskrivning för kommandot sätts, transactionService används
+        super("radera", "Radera en befintlig transaktion", transactionService);
     }
 
     @Override
-    public void execute() {
+    public void execute() throws Exception {
+        // Metoden för att köra kommandot
 
-        Scanner scan = new Scanner(System.in);
+        // Hämtar samtliga transaktioner
+        List<Transaction> transactions = transactionService.findAll();
+
+        // Formatterar om datumformat
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
+        // Scanner deklareras
+        Scanner scan = new Scanner(System.in);
+
+
+        // Om listan är tom så skrivs en utskrift till användaren om detta
         if (transactions.isEmpty()){
-            System.out.println("Inga transaktioner att ta");
+            System.out.println("Inga transaktioner att hämta");
             return;
         }
 
-
+        // Loopar genom listan
         for (Transaction t : transactions){
 
+            // Printar alla transaktioner
             System.out.println("ID " + t.getID() +
                     ", Beskrivning: " + t.getDescription() +
                     ", Belopp: " + t.getAmount() +
-                    ", Datum: " + t.getDateTime().format(formatter));
+                    ", Datum: " + t.getDate().format(formatter) +
+                    ", Typ: " + t.getType());
 
         }
 
-
+        // Utskrift till användaren
         System.out.println("Ange ID för den transaktionen du vill ta bort");
-        int idToDelete = scan.nextInt();
 
-        Transaction transactionRemove = null;
-        for (Transaction t : transactions) {
-            if (idToDelete == t.getID()) {
+        // Tar emot ID för transaktionen genom scanner
+        String idToDelete = scan.nextLine();
 
-                transactionRemove = t;
-                break;
+        // Try catch
+        try {
+            // Tar emot ID
+            UUID id = UUID.fromString(idToDelete);
+
+            // Genom transactionService så raderas transaktionen
+            transactionService.delete(id);
+
+            // Utskrift till användaren
+            System.out.println("Transaktionen " + id + " har raderats från systemet");
 
 
-            }
+        } catch (IllegalArgumentException e) {
+
+            // Felmeddelande skrivs ut
+            System.out.println("Felaktigt ID-format! Försök igen");
+
         }
-
-        if (transactionRemove != null){
-            transactions.remove(transactionRemove);
-            System.out.println("Transaktion med ID" + idToDelete + " togs bort.");
-        } else {
-
-            System.out.println("Ingen transaktion hittades med ID " + idToDelete + ".");
-
-        }
-
     }
 }
