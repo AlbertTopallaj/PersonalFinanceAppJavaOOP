30de9541662362935b915409bb0fea1633ac4510
Professor <superelitealfa@gmail.com>
2025-11-06 20:55:23 +0100

    Changed all variabels from protected to private, for more OOP, added comments for clarity

--- src/Transaction/Transaction.java
+++ src/Transaction/Transaction.java
@@ -1,26 +1,50 @@
 package Transaction;
 
+import services.TransactionService;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 public class Transaction {
 
+    // Formattera datumformat
     public static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
-    protected UUID ID;
-    protected String description;
-    protected int amount;
-    protected LocalDateTime date;
-    protected boolean isIncome;
+    // ID med UUID
+    private final UUID ID;
 
-    public Transaction(String description, int amount, LocalDateTime date, boolean isIncome){
+    // Beskrivning
+    private String description;
+
+    // Belopp
+    private int amount;
+
+    // Datum
+    private LocalDateTime date;
+
+    // Typ
+    private boolean isIncome;
+
+    // Konstruktor för transaktionen med genererat UUID
+    public Transaction(String description, int amount, LocalDateTime date, boolean isIncome) {
         this.ID = UUID.randomUUID();
         this.description = description;
         this.amount = amount;
         this.date = date;
         this.isIncome = isIncome;
     }
+
+    // Konstruktor med färdig genererat UUID
+    public Transaction(UUID ID, String description, int amount, LocalDateTime date, boolean isIncome) {
+        this.ID = ID;
+        this.description = description;
+        this.amount = amount;
+        this.date = date;
+        this.isIncome = isIncome;
+    }
+
+    // Getters och setters för samtliga variabler
 
     public UUID getID() {
         return ID;
@@ -34,7 +58,7 @@ public class Transaction {
         return description;
     }
 
-    public void setAmount(int amount){
+    public void setAmount(int amount) {
         this.amount = amount;
     }
 
@@ -57,12 +81,5 @@ public class Transaction {
     public void setIncome(boolean income) {
         isIncome = income;
     }
-
-    @Override
-    public String toString() {
-
-        return ID + description + amount + date + isIncome;
-
-
-    }
 }
+
