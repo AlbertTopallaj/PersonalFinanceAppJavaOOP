18a11e9c370e7d70f6130de3eec1585263ce5ade
Professor <superelitealfa@gmail.com>
2025-11-06 20:53:12 +0100

    declared services, all commands now registers in main, added comments for clarity, main starts the Login class so users can login

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,41 @@
+import commands.CreateTransactionCommand;
+import commands.DeleteTransactionCommand;
+import commands.ExitApplicationCommand;
+import commands.ListTransactionCommand;
 import models.Login;
+import repositories.FileTransactionRepository;
+import services.ApplicationService;
+import services.ITransactionService;
+import services.TerminalCommandService;
+import services.TransactionService;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        Login login = new Login(); // Starta Login objektet
 
-        Login login = new Login();
+        if (login.authenticate()) { // Om man lyckas logga in korrekt
 
-        login.run();
+            // Skapa objekt samt deklarera samtliga objekt
 
+            // Deklarera transactionService samt FileTransactionRepository
+            ITransactionService transactionService = new TransactionService(new FileTransactionRepository());
+
+            // Deklarera applicationService
+            ApplicationService applicationService = new ApplicationService(transactionService);
+
+
+            // Registera samtliga kommandon
+            applicationService.registerCommand(new CreateTransactionCommand(transactionService));
+            applicationService.registerCommand(new ListTransactionCommand(transactionService));
+            applicationService.registerCommand(new DeleteTransactionCommand(transactionService));
+            applicationService.registerCommand(new ExitApplicationCommand(transactionService));
+
+            // Deklarera TerminalCommandService
+            TerminalCommandService terminalCommandService = new TerminalCommandService(applicationService);
+
+            // TerminalCommandService startas då den har en start metod
+            terminalCommandService.start();
+
+        }
     }
 }
