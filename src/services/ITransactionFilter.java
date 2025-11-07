2f1dd73d2b02cd49e50f32d968ef634a3584a4a9
Professor <superelitealfa@gmail.com>
2025-11-07 01:42:01 +0100

    added a filter transaction interface for holding lists to the filter, and to increase usage of OOP structure

---
+++ src/services/ITransactionFilter.java
@@ -0,0 +1,20 @@
+package services;
+
+import Transaction.Transaction;
+
+import java.time.LocalDate;
+import java.time.Year;
+import java.time.YearMonth;
+import java.util.List;
+
+public interface ITransactionFilter {
+
+    // Interface som håller i filtrets funktioner
+
+    List<Transaction> filterByType(List<Transaction> transactions, boolean isIncome);
+    List<Transaction> filterByDay(List<Transaction> transactions, LocalDate day);
+    List<Transaction> filterByWeek(List<Transaction> transactions, LocalDate week);
+    List<Transaction> filterByMonth(List<Transaction> transactions, YearMonth month);
+    List<Transaction> filterByYear(List<Transaction> transactions, int year);
+
+}
